1a8747b17024c379c07225ddf4eecd7d8b3e93cc
hmk1022 <hm.kim@iworkerman.com>
2022-01-18 08:56:05 +0900

    add-ui

--- memento_RN/src/components/Navigation.js
+++ memento_RN/src/components/Navigation.js
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {actionCreators as steemActions} from '../../reducers/steem'
+import AppBar from "@material-ui/core/AppBar";
+import { BottomNavigation, BottomNavigationAction  } from '@mui/material';
+import { RestoreIcon, FavoriteIcon, LocationOnIcon } from "@material-ui/icons";
 
 function Navigation() {
     const steem = useSelector((state) => state.steem.feeds);
@@ -17,9 +20,17 @@ function Navigation() {
 
     console.log('상태값2 : ',steem)
     return (
-        <div>
-            dddd
-        </div>
+        <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+            setValue(newValue);
+        }}
+        >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
     )
 }
 
